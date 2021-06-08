76bca37a7854b31b5813a664a53ae01ab0f4002f
unknown <oseparovic@gmail.com>
2021-06-08 08:57:41 -0700

    vfk prefix removed from prefs

--- library/src/main/java/com/appstronautstudios/voteforkey/VoteForKey.java
+++ library/src/main/java/com/appstronautstudios/voteforkey/VoteForKey.java
@@ -4,20 +4,16 @@ import android.app.Activity;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
-import com.appstronautstudios.voteforkey.utils.Constants;
-
 public class VoteForKey {
     public static String getVoteForKey(Activity activity, String key) {
-        String keyWithPrefix = Constants.PREF_KEY_PREFIX + "_" + key;
         SharedPreferences preferenceManager = PreferenceManager.getDefaultSharedPreferences(activity);
-        return preferenceManager.getString(keyWithPrefix, null);
+        return preferenceManager.getString(key, null);
     }
 
     public static void setVoteForKey(Activity activity, String key, String vote) {
-        String keyWithPrefix = Constants.PREF_KEY_PREFIX + "_" + key;
         SharedPreferences preferenceManager = PreferenceManager.getDefaultSharedPreferences(activity);
         SharedPreferences.Editor editor = preferenceManager.edit();
-        editor.putString(keyWithPrefix, vote);
+        editor.putString(key, vote);
         editor.apply();
     }
 
--- library/src/main/java/com/appstronautstudios/voteforkey/utils/Constants.java
+++ library/src/main/java/com/appstronautstudios/voteforkey/utils/Constants.java
@@ -2,5 +2,5 @@ package com.appstronautstudios.voteforkey.utils;
 
 public class Constants {
 
-    public static final String PREF_KEY_PREFIX = "vfk";
+    public static final String PREF_KEY_PREFIX = "vfk"; // unused
 }
